c97bcbce10ad4e6a4c3fe4ec6cf891132b54eb4c
Kevin Conklin <kevinskyhighhh@hotmail.com>
2016-08-22 01:31:32 -0400

    added in first slide and began designing 2nd slide

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
 var express = require('express'),
-    logger  = require('morgan')('dev'),
-    server  = express();
+    logger = require('morgan')('dev'),
+    server = express();
 
-server.use(express.static(__dirname+'/public'));
+
+//Static Ass like HTML, JS, and CSS
+server.use(express.static(__dirname+'/'));
+//Setup Logging
 server.use(logger);
-server.set('port', process.env.PORT || 8080);
 
-server.get('/', home);
+server.get('/', function(req, res){
+  res.sendFile('/index.html', {root:__dirname})
+});
 
-server.listen(server.get('port'), listenCallback);
-
-function home(req, res){
-  res.sendFile('/html/index.html', {root: __dirname+'/public'});
-}
-
-function listenCallback(){
-console.log('Now listening on port ' + server.get('port'));
-
-}
+server.listen(8080, function(){
+  console.log("May the Force Be With You...");
+});
